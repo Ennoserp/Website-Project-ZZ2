9409910126b641d0ab38dc52159a3a6656de3c20
Jael <39562622+Ennoserp@users.noreply.github.com>
2020-02-10 06:01:45 +0100

    major update

---
+++ vente_en_lignes/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Main page</title>
+    </head>
+
+    <body>
+        <?php include("Projet.php"); ?>
+    </body>
+</html>
